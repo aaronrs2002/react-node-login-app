8c72d4eb99266eca8205f318002da8b61b44986b
Aaron Smith <aaronrs2002@Aarons-Mac-mini-2.local>
2020-11-04 08:19:12 -0700

    try catch replce

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ import DeleteUser from "./components/DeleteUser";
 import Theme from "./components/Theme";
 import ChangePassword from "./components/ChangePassword";
 import uuid from "./components/uuid";
-import SaveTheme from "./components/SaveTheme"
+import SaveTheme from "./components/SaveTheme";
 import MainContent from "./components/MainContent";
-import Nav from "./components/Nav"
+import Nav from "./components/Nav";
 
 function App() {
   let [userEmail, setEmail] = useState(null);
@@ -26,12 +26,12 @@ function App() {
   };
 
   const showAlert = (theMessage, theType) => {
-    setAlertType((alertType) => theType)
+    setAlertType((alertType) => theType);
     setAlert((alert) => theMessage);
     setTimeout(() => {
       setAlert((alert) => "default");
     }, 5000);
-  }
+  };
 
   //START VALIDATE USER
   const validateUser = (success, token, email, msg) => {
@@ -42,23 +42,23 @@ function App() {
       setTokenCk((checkedToken) => true);
       setEmail((userEmail) => email);
       sessionStorage.setItem("email", email);
-        axios.get("/theme/" + email).then(
-          (res) => {
-            if (res.data[0].theme) {
-              SaveTheme(res.data[0].theme);
-            } else {
-              SaveTheme("css/bootstrap.min.css");
-            }
-          },
-          (error) => {
-            console.log(error);
+      axios.get("/theme/" + email).then(
+        (res) => {
+          if (res.data[0].theme) {
+            SaveTheme(res.data[0].theme);
+          } else {
+            SaveTheme("css/bootstrap.min.css");
           }
-        );
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     } else {
       setValidUser((isValidUser) => false);
       setToken((token) => token);
       setEmail((userEmail) => null);
-      showAlert( "That didn't work: " + msg, "danger");
+      showAlert("That didn't work: " + msg, "danger");
     }
   };
   ///END VALIDATE USER
@@ -81,12 +81,12 @@ function App() {
       )
       .then(
         (res) => {
-          showAlert( "User created succussfully!", "success");
+          showAlert("User created succussfully!", "success");
           setUserType((newUser) => false);
           document.querySelector("button.ckValidate").classList.remove("hide");
         },
         (error) => {
-          showAlert( "That didn't work: " + error, "danger");
+          showAlert("That didn't work: " + error, "danger");
         }
       );
   };
@@ -98,8 +98,8 @@ function App() {
     const email = document.querySelector("input[name='email']").value;
     const password = document.querySelector("input[name='password']").value;
 
-    if(!email || !password){
-      showAlert( 'Not one character?', "danger");
+    if (!email || !password) {
+      showAlert("Not one character?", "danger");
       return false;
     }
 
@@ -118,7 +118,7 @@ function App() {
           validateUser(res.data.success, res.data.token, email, res.data.data);
         },
         (error) => {
-          showAlert( "That didn't work: " + error, "danger");
+          showAlert("That didn't work: " + error, "danger");
         }
       );
   };
@@ -130,106 +130,125 @@ function App() {
     sessionStorage.removeItem("token");
     sessionStorage.removeItem("email");
 
-    axios.put(
-      "/logout-uuid/",
-      {
+    axios
+      .put("/logout-uuid/", {
         email: userEmail,
-        uuid: "logged-out: "+ uuid(),
-      }
-    ).then(
-      (res) => {
-      console.log("res from logout: "+ res);
-      },
-      (error) => {
-        showAlert( "Something happened while logging out: : " + error, "danger");
-      }
-    );
+        uuid: "logged-out: " + uuid(),
+      })
+      .then(
+        (res) => {
+          console.log("res from logout: " + res);
+        },
+        (error) => {
+          showAlert(
+            "Something happened while logging out: : " + error,
+            "danger"
+          );
+        }
+      );
   };
   //END LOG OUT
 
   //START REFRESH
   useEffect(() => {
+    console.log("checkedToken: " + checkedToken);
     if (sessionStorage.getItem("token") && checkedToken === false) {
-      axios.get("/check-token/" + sessionStorage.getItem("email"),
-      config).then(
+      axios.get("/check-token/" + sessionStorage.getItem("email"), config).then(
         (res) => {
-          if (sessionStorage.getItem("token") === res.data[0].token) {
-            validateUser(
-              1,
-              res.data[0].token,
-              sessionStorage.getItem("email"),
-              "token success"
-            );
-          } else {
-            console.log(
-              "The tokens did not match - sessionStorage.getItem('token') " +
-                sessionStorage.getItem("token") +
-                " res.data[0].token: " +
-                res.data[0].token
-            );
+          try {
+            if (sessionStorage.getItem("token") === res.data[0].token) {
+              validateUser(
+                1,
+                res.data[0].token,
+                sessionStorage.getItem("email"),
+                "token success"
+              );
+            }
+          } catch (error) {
+            console.error(error);
+            return false;
           }
         },
         (error) => {
-          showAlert( "That didn't work: " + error, "danger");
+          showAlert("That token-request didn't work: " + error, "danger");
+          logout();
         }
       );
-    } 
+    }
   });
   //END REFRESH
 
   return (
     <React.Fragment>
       {isValidUser === true ? (
-<Nav />
-      ) : 
-      <Login
-            login={login}
-            createUser={createUser}
-            setUserType={setUserType}
-            newUser={newUser}
-          />
-      }
+        <Nav />
+      ) : (
+        <Login
+          login={login}
+          createUser={createUser}
+          setUserType={setUserType}
+          newUser={newUser}
+        />
+      )}
       <main>
-      <div className={isValidUser !== false ? "container py-5": "container"} >
-
-      {isValidUser === true ? (
-        <MainContent />
-        ):null
-      }
+        {" "}
         {alert !== "default" ? (
           <div
-            className={"alert alert-" + alertType + " animated fadeIn"}
+            className={"alert alert-" + alertType + " animated fadeInDown"}
             role="alert"
           >
             {alert}
           </div>
         ) : null}
-      </div>
+        <div className={isValidUser !== false ? "container py-5" : "container"}>
+          {isValidUser === true ? <MainContent /> : null}
+        </div>
       </main>
-  {isValidUser === true ? (<footer className="footer mt-auto py-3 px-3 bg-dark text-muted">
+      {isValidUser === true ? (
+        <footer className="footer mt-auto py-3 px-3 bg-dark text-muted">
           <div className="row">
             <div className="col-md-3">
-{infoMessage === "account-settings" ? 
-<a href="#settingsPanel"  className="btn btn-secondary" onClick={() => toggleInfo((infoMessage) => "")}>{userEmail} <i className="fas fa-cog"></i></a>:
-<a href="#settingsPanel" className="btn btn-secondary" onClick={() => toggleInfo((infoMessage) => "account-settings")}>{userEmail} <i className="fas fa-cog"></i></a>
-}
-{infoMessage === "account-settings" ? 
-              <div id="settingsPanel" className="py-2">
-              <label>Settings:</label>
-              <ul className="noStyle">
-                <li> <Theme userEmail={userEmail} showAlert={showAlert} /></li>
-                <li><ChangePassword showAlert={showAlert} /></li>
-                <li>
-                  <DeleteUser
-                            userEmail={userEmail}
-                            logout={logout}
-                            showAlert={showAlert}
-                            infoMessage={infoMessage}
-                          />
-                  </li>
-              </ul>
-              </div>
-              :null}
+              {infoMessage === "account-settings" ? (
+                <a
+                  href="#settingsPanel"
+                  className="btn btn-secondary"
+                  onClick={() => toggleInfo((infoMessage) => "")}
+                >
+                  {userEmail} <i className="fas fa-cog"></i>
+                </a>
+              ) : (
+                <a
+                  href="#settingsPanel"
+                  className="btn btn-secondary"
+                  onClick={() =>
+                    toggleInfo((infoMessage) => "account-settings")
+                  }
+                >
+                  {userEmail} <i className="fas fa-cog"></i>
+                </a>
+              )}
+              {infoMessage === "account-settings" ? (
+                <div id="settingsPanel" className="py-2">
+                  <label>Settings:</label>
+                  <ul className="noStyle">
+                    <li>
+                      {" "}
+                      <Theme userEmail={userEmail} showAlert={showAlert} />
+                    </li>
+                    <li>
+                      <ChangePassword showAlert={showAlert} />
+                    </li>
+                    <li>
+                      <DeleteUser
+                        userEmail={userEmail}
+                        logout={logout}
+                        showAlert={showAlert}
+                        infoMessage={infoMessage}
+                      />
+                    </li>
+                  </ul>
+                </div>
+              ) : null}
             </div>
             <div className="col-md-7"></div>
             <div className="col-md-2">
@@ -238,8 +257,8 @@ function App() {
               </button>
             </div>
           </div>
-          </footer>
-        ):null}{" "}
+        </footer>
+      ) : null}{" "}
     </React.Fragment>
   );
 }
